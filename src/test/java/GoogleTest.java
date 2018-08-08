b5baa46dc0f9454589551489b19eb807d21abb4f
Viktoriia <vs.pokhylko@gmail.com>
2018-08-08 10:06:18 +0300

    Initial commit

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,14 +1,21 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleTest {
     @Test
     public void userCanSearch() {
-        open("https://google.com/ncr");
+        open("https://google.com/");
         new GooglePage().searchFor("Java");
 
         SearchResultPage results = new SearchResultPage();
-        results.getResult(0).shouldHave(text("Java: Computer programming language"));
+        results.getResult(0).shouldHave(text("Computer programming language"));
+    }
+
+    @Test
+    public void testApp() {
+        App app = new App();
+        app.logMessages();
     }
 }
